515b4b45b7a30911866447e8762b5112eb04f4c2
garimabhayanaa <garimaabhayanaa@gmail.com>
2024-06-30 17:05:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDom from "react-dom";
 
-function App() {
+function Next(params) {
+  const nextPeriodDate = new Date(params.lastDate);
+  nextPeriodDate.setDate(
+    nextPeriodDate.getDate() + parseInt(params.cycleDuration, 10)
+  );
+  const formattedDate = nextPeriodDate.toLocaleDateString();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h2 id="info">Your period will begin on or around {formattedDate}.</h2>
+  );
+}
+function Ovulation(params) {
+  const ovDate1 = new Date(params.lastDate);
+  ovDate1.setDate(ovDate1.getDate() + parseInt(params.cycleDuration, 10) - 16);
+  const formattedDate1 = ovDate1.toLocaleDateString();
+  const ovDate2 = new Date(params.lastDate);
+  ovDate2.setDate(ovDate2.getDate() + parseInt(params.cycleDuration, 10) - 12);
+  const formattedDate2 = ovDate2.toLocaleDateString();
+  return (
+    <h2 id="info">
+      Most fertile window from {formattedDate1} to {formattedDate2}.
+    </h2>
   );
 }
 
-export default App;
+export { Next,Ovulation};
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom";
+import moment from "moment";
+import { Next, Ovulation } from "./App.js";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDom.render(
+  <div>
+    <div id="calendar">
+      <h2 id="date_prompt">Enter last period date: </h2>
+      <button id="dbutton">
+        <input type="date" id="Test_Date" />
+      </button>
+    </div>
+    <div id="prompt2"></div>
+    <div id="choices"></div>
+    <button id="next" class="buttons">
+      Expected Period Date
+    </button>
+    <button id="ovulation" className="buttons">
+      Possible Ovulation Window
+    </button>
+  </div>,
+  document.getElementById("root")
 );
+var date;
+var cycleLength;
+document.getElementById("Test_Date").addEventListener("change", function () {
+  date = new Date(this.value);
+  console.log(date);
+  ReactDom.render(
+    <div id="cycleDuration">
+      <br />
+      <h2 id="cycleInfo">Enter average cycle length: </h2>
+      <input id="duration" type="number"></input>
+    </div>,
+    document.getElementById("prompt2")
+  );
+  document.getElementById("duration").addEventListener("input", function () {
+    cycleLength = this.value;
+  });
+});
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+document.getElementById("next").addEventListener("click", function () {
+  ReactDom.render(
+    <Next lastDate={date} cycleDuration={cycleLength} />,
+    document.getElementById("choices")
+  );
+});
+document.getElementById("ovulation").addEventListener("click", function () {
+  ReactDom.render(
+    <Ovulation lastDate={date} cycleDuration={cycleLength} />,
+    document.getElementById("choices")
+  );
+});
